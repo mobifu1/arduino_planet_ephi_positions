69441695357d7bb8cdc4afe0739dde0ff0fa160b
Mobifu1 <aajahanke@aol.com>
2016-12-23 17:47:06 +0100

    jukian date Signed-off-by: Mobifu1 <aajahanke@aol.com>

--- Planet_ephi_positions.ino
+++ Planet_ephi_positions.ino
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------
 // Calculation the Positions of Planets by elliptic Orbit in the Solarsystem between Year 2000-2100
+// http://denknix.com/astro/doc/html/section003.html
 // Author: Andreas Jahnke, aajahnke@aol.com
 //------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------
@@ -15,7 +16,7 @@
 //   ω      Winkel zwischen Perihel und aufsteigendem Knoten
 
 // Tables:
-const int day_of_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int day_of_month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 String planet_name[8] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun"};
 String star_name[1] = {"Sun"};
 
@@ -32,6 +33,9 @@ const float planet_data[8][7] = {// a, e, t0, T, Ω, i, ω
 
 void setup() {
 
+  Serial.begin(9600);
+  float jd = julian_date_now (23, 12, 2016, 12, 00);
+  Serial.println("JD:" + String(jd));
 
 }
 
@@ -39,3 +43,32 @@ void loop() {
 
 
 }
+//------------------------------------------------------------------------------------------------------------------
+float julian_date_now (float day_, float month_, float year_, float hour_, float minute_) { // UTC
+
+  float jd = 0;
+  float ut = (hour_ + (minute_ / 60));
+
+  jd = julian_date_0(year_);
+  jd += days_to_month(month_) + day_;
+  jd += (ut - 12) / 24;
+  return jd;
+};
+//------------------------------------------------------------------------------------------------------------------
+float julian_date_0(float year_) {
+
+  float jd0 = (4712 + year_) * 365.24806317;
+  Serial.println("JD0:" + String(jd0));//2457389 = 1.1.2016 12:00 UTC / 2451545 = 1.1.2000 12:00 UTC
+  return jd0;
+};
+//------------------------------------------------------------------------------------------------------------------
+float days_to_month(float month_) {
+
+  float result = 0;
+  for (int m = 1; m < (int)month_; m++) {
+    result += (float)day_of_month[m];
+  }
+  return result;
+};
+//------------------------------------------------------------------------------------------------------------------
+
