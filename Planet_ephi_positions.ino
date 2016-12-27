3261006179ab3324f334d02f9b86923699cd97a7
Mobifu1 <aajahanke@aol.com>
2016-12-27 21:42:18 +0100

    add code Signed-off-by: Mobifu1 <aajahanke@aol.com>

--- Planet_ephi_positions.ino
+++ Planet_ephi_positions.ino
@@ -7,7 +7,7 @@
 //------------------------------------------------------------------------------------------------------------------
 
 // Sechs Bahnelemente:                                                                 / Variable:
-// a: Länge der großen Halbachse                                                       / [0] semi major axis
+// a: Länge der großen Halbachse                                                       / [0] semi major axis in AE
 // e: numerische Exzentrizität                                                         / [2] eccentricity
 // i: Bahnneigung, Inklination                                                         / [4] inclination
 // L                                                                                   / [6] meanLongitude
@@ -77,7 +77,7 @@ void get_object_position (int object_number, float jd) {
   Serial.println("Object:" + object_name[object_number]);
 
   float T = (jd - 2451545) / 36525.0;
-  Serial.println("T:" + String(T));
+  Serial.println("T:" + String(T, DEC));
 
   float semiMajorAxis = object_data[object_number][0] + (T * object_data[object_number][1]); // offset + T * delta
   float eccentricity = object_data[object_number][2] + (T * object_data[object_number][3]);
@@ -88,31 +88,125 @@ void get_object_position (int object_number, float jd) {
   float meanAnomaly = meanLongitude - longitudePerihelion;
   float argumentPerihelion = longitudePerihelion - longitudeAscendingNode;
 
-  if (semiMajorAxis >= 360)semiMajorAxis = modulo_360 (semiMajorAxis);
-  Serial.println("semiMajorAxis:" + String(semiMajorAxis));
-  if (eccentricity >= 360)eccentricity = modulo_360 (eccentricity);
-  Serial.println("eccentricity:" + String(eccentricity));
-  if (inclination >= 360)inclination = modulo_360 (inclination);
-  Serial.println("inclination:" + String(inclination));
-  if (meanLongitude >= 360)meanLongitude = modulo_360 (meanLongitude);
-  Serial.println("meanLongitude:" + String(meanLongitude));
-  if (longitudePerihelion >= 360)longitudePerihelion = modulo_360 (longitudePerihelion);
-  Serial.println("longitudePerihelion:" + String(longitudePerihelion));
-  if (longitudeAscendingNode >= 360)longitudeAscendingNode = modulo_360 (longitudeAscendingNode);
-  Serial.println("longitudeAscendingNode:" + String(longitudeAscendingNode));
-  if (meanAnomaly >= 360)meanAnomaly = modulo_360 (meanAnomaly);
-  Serial.println("meanAnomaly:" + String(meanAnomaly));
-  if (argumentPerihelion >= 360)argumentPerihelion = modulo_360 (argumentPerihelion);
-  Serial.println("argumentPerihelion:" + String(argumentPerihelion));
+  Serial.println("semiMajorAxis:" + String(semiMajorAxis, DEC));
+  Serial.println("eccentricity:" + String(eccentricity, DEC));
+  inclination = calc_modulo_360 (inclination);
+  Serial.println("inclination:" + String(inclination, DEC));
+  meanLongitude = calc_modulo_360 (meanLongitude);
+  Serial.println("meanLongitude:" + String(meanLongitude, DEC));
+  longitudePerihelion = calc_modulo_360 (longitudePerihelion);
+  Serial.println("longitudePerihelion:" + String(longitudePerihelion, DEC));
+  longitudeAscendingNode = calc_modulo_360 (longitudeAscendingNode);
+  Serial.println("longitudeAscendingNode:" + String(longitudeAscendingNode, DEC));
+  meanAnomaly = calc_modulo_360 (meanAnomaly);
+  Serial.println("meanAnomaly:" + String(meanAnomaly, DEC));
+  argumentPerihelion = calc_modulo_360 (argumentPerihelion);
+  Serial.println("argumentPerihelion:" + String(argumentPerihelion, DEC));
+
+  float eccentricAnomaly = calc_eccentricAnomaly(meanAnomaly, eccentricity);
+  eccentricAnomaly = calc_modulo_360 (eccentricAnomaly);
+  Serial.println("eccentricAnomaly:" + String(eccentricAnomaly, DEC));
+
+  orbitalCoordinates (semiMajorAxis, eccentricity, eccentricAnomaly);
+}
+//------------------------------------------------------------------------------------------------------------------
+float calc_modulo_360 (float value) {
+
+  if (value >= 360 || value < 0) {
+    if (value < 0) {
+      value += 360000;
+    }
+    long x = (long)value;
+    float comma = value - x;
+    long y = x % 360;
+    return comma += y;
+  }
+  return value;
+}
+//------------------------------------------------------------------------------------------------------------------
+float calc_eccentricAnomaly (float meanAnomaly, float eccentricity) { //271.60 deg   /  0.00677672 rad
+
+  meanAnomaly *= rad;
+
+  float eccentricAnomaly = meanAnomaly + (eccentricity * sin(meanAnomaly));
+  float deltaEccentricAnomaly = 1;
+
+  while (abs(deltaEccentricAnomaly) > 0.0000001) {
+    deltaEccentricAnomaly = (meanAnomaly - eccentricAnomaly + eccentricity * sin(eccentricAnomaly)) / (1 - eccentricity * cos(eccentricAnomaly));
+    //Serial.println(String(deltaEccentricAnomaly, DEC));
+    eccentricAnomaly += deltaEccentricAnomaly;
+  }
+
+  eccentricAnomaly *= deg;
+  return eccentricAnomaly; //174.3823298500084
+}
+//------------------------------------------------------------------------------------------------------------------
+void orbitalCoordinates (float semiMajorAxis, float eccentricity, float eccentricAnomaly) {
+
+  eccentricAnomaly *= rad;
+  float lambda = 2 * atan(sqrt((1 + eccentricity) / (1 - eccentricity)) * tan(eccentricAnomaly / 2));
+  lambda *= deg;
+  lambda = calc_modulo_360 (lambda);
+
+  float radius = semiMajorAxis * (1 - eccentricity * cos(eccentricAnomaly));
+  Serial.println("lambda:" + String(lambda, DEC));
+  Serial.println("radius:" + String(radius, DEC));
+
+  calc_vector(0, lambda, radius, "spherical"); // x=beta / y=lambda / z=r
 
 }
 //------------------------------------------------------------------------------------------------------------------
-float modulo_360 (float value) {
+void calc_vector(float x, float y, float z, String mode) { // x = beta / y = lambda / z = radius
 
-  long x = (long)value;
-  float comma = value - x;
-  long y = x % 360;
-  return comma += y;
+  x *= rad;
 
+  if (mode == "cartesian") {
+  }
+  if (mode == "spherical") {
+    // x = beta / y = lambda / z = radius
+    x = z * cos(x) * cos(y);
+    y = z * cos(x) * sin(y);
+    z = z * sin(x);
+  }
+
+  //getLongitude:
+  float lon;
+  if (x > 0 && y > 0) lon = atan(y / x);
+  if (x > 0 && y < 0) lon =  2 * pi + atan(y / x);
+  if (x > 0 && y == 0)  lon =  0;
+
+  if (x == 0 && y > 0)lon =  pi / 2;
+  if (x == 0 && y < 0) lon =  3 * pi / 2;
+  if (x == 0 && y == 0) lon =  0;
+
+  if (x < 0 && y > 0)lon =  pi + atan(y / x);
+  if (x < 0 && y < 0)lon =  pi + atan(y / x);
+  if (x < 0 && y == 0) lon =  pi;
+  //lon *= deg;
+  lon = calc_modulo_360 (lon);
+  Serial.println("LON:" + String(lon, DEC));
+
+  //getLatitude:
+  float rho = sqrt(x * x + y * y);
+  float lat;
+  if (rho != 0) {
+    lat = atan(z / rho);
+  }
+  else {
+    if (z < 0)
+      lat =  -pi / 2;
+    if (z > 0)
+      lat =  pi / 2;
+    if (z == 0)
+      lat =  0;
+  }
+  //lat *= deg;
+  lat = calc_modulo_360 (lat);
+  Serial.println("LAT:" + String(lat, DEC));
+  //getDistance:
+
+  float dist = sqrt(x * x + y * y + z * z);
+  Serial.println("DIS:" + String(dist, DEC));
 }
+//------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------
